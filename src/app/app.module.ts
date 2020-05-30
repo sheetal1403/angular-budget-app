807f2495c762e5690b5a762e927e5c0998765e11
Sheetal R <sheetalraghupathi14@gmail.com>
2020-05-30 16:43:07 +0530

    =List components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { AddItemFormComponent } from './add-item-form/add-item-form.component';
-import { BudgetItemListComponent } from './budget-item-list/budget-item-list.component';
+import { BudgetListComponent } from './budget-list/budget-list.component';
+import { BudgetItemCardComponent } from './budget-list/budget-item-card/budget-item-card.component';
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     MainPageComponent,
     AddItemFormComponent,
-    BudgetItemListComponent
+    BudgetListComponent,
+    BudgetItemCardComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/budget-list/budget-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-budget-list',
+  templateUrl: './budget-list.component.html',
+  styleUrls: ['./budget-list.component.scss']
+})
+export class BudgetListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
